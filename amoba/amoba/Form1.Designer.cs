fae3713ee7f6d02d5f1b7a1cebadf53f470217d5
vataiadrian <vataiadrian@gmail.com>
2021-10-07 11:30:58 +0200

    kezdő form #3

--- amoba/amoba/Form1.Designer.cs
+++ amoba/amoba/Form1.Designer.cs
@@ -39,7 +39,7 @@ namespace amoba
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(84, 110);
+            this.button1.Location = new System.Drawing.Point(49, 114);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(141, 67);
             this.button1.TabIndex = 0;
@@ -53,9 +53,9 @@ namespace amoba
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(164, 25);
+            this.label1.Size = new System.Drawing.Size(97, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Játékos 1 neve:";
+            this.label1.Text = "Player 1:";
             // 
             // label2
             // 
@@ -63,14 +63,14 @@ namespace amoba
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.Location = new System.Drawing.Point(12, 57);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(164, 25);
+            this.label2.Size = new System.Drawing.Size(97, 25);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Játékos 2 neve:";
+            this.label2.Text = "Player 2:";
             // 
             // jatekosnev1tb
             // 
             this.jatekosnev1tb.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.jatekosnev1tb.Location = new System.Drawing.Point(182, 6);
+            this.jatekosnev1tb.Location = new System.Drawing.Point(115, 9);
             this.jatekosnev1tb.Name = "jatekosnev1tb";
             this.jatekosnev1tb.Size = new System.Drawing.Size(100, 31);
             this.jatekosnev1tb.TabIndex = 3;
@@ -79,7 +79,7 @@ namespace amoba
             // jatekosnev2tb
             // 
             this.jatekosnev2tb.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.jatekosnev2tb.Location = new System.Drawing.Point(182, 54);
+            this.jatekosnev2tb.Location = new System.Drawing.Point(115, 54);
             this.jatekosnev2tb.Name = "jatekosnev2tb";
             this.jatekosnev2tb.Size = new System.Drawing.Size(100, 31);
             this.jatekosnev2tb.TabIndex = 4;
@@ -89,7 +89,7 @@ namespace amoba
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(313, 206);
+            this.ClientSize = new System.Drawing.Size(240, 206);
             this.Controls.Add(this.jatekosnev2tb);
             this.Controls.Add(this.jatekosnev1tb);
             this.Controls.Add(this.label2);
